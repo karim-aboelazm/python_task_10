2a1112a1d695afc2f2fe511d767299a37e780f68
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2021-08-17 01:51:23 +0200

    first commit

---
+++ Boolean operators/task.md
@@ -0,0 +1,10 @@
+## Boolean operators
+
+Boolean is a type of value that can only be `True` or `False`. The `==` (equality) operator 
+compares two variables and checks whether they are equal.  
+
+  
+Check whether the variable `two` is equal to `three`.
+
+
+<div class='hint'>Use the <code>==</code> operator.</div>
---
+++ Variable definition/variable_definition.py
@@ -0,0 +1,11 @@
+# This is called a "chained assignment". It assigns the value 2 to variables "a" and "b".
+a = b = 2
+# We'll explain the expression str(a) later in the course.
+# For now: it is used to convert the variable "a" into a string.
+print("a = " + str(a))
+print("b = " + str(b))
+
+greetings = "greetings"
+print("greetings = " + str(greetings))
+greetings = '???'
+print("greetings = " + str(greetings))
